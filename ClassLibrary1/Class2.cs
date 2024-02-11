910e7a6c8603c056c8b3dfdaae531f4409cf1d72
Notmisa09 <125936102+Notmisa09@users.noreply.github.com>
2024-02-11 19:58:46 -0400

    Age

---
+++ ClassLibrary1/Class2.cs
@@ -0,0 +1,8 @@
+﻿namespace ClassLibrary1
+{
+    public class Class2
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
--- ClassLibrary1/obj/Release/net8.0/ClassLibrary1.AssemblyInfo.cs
+++ ClassLibrary1/obj/Release/net8.0/ClassLibrary1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56451710c2ea2683aeac3057fdc3c2e1cf90edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
